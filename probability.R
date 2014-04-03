438e768d7c897a5f73917590f7edfb957967bec2
Mike Hurd <mike@mhurd.com>
2014-04-03 13:00:28 +0100

    Added algorithms script
    Including Fermat's small theorem of testing for primeness

--- probability.R
+++ probability.R
@@ -1,3 +1,4 @@
 diceThrowsHist <- function(n) {
+  colors=c("red", "yellow", "blue", "green", "purple", "brown")
   hist(breaks=seq(0,6,1),sample(1:6,n,replace=T), col=colors, main=paste(n," Dice Throws"), xlab="Dice Throw")
 }
